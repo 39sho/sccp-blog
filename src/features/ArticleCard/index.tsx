0670b5ad217027e73013c27fec385f66d2cc6716
39sho <g.39sho@gmail.com>
2023-08-07 16:23:41 +0900

    w-full -> w-11/12

--- src/features/ArticleCard/index.tsx
+++ src/features/ArticleCard/index.tsx
@@ -2,7 +2,7 @@ type props = { url: string, title: string, date: Date, children: string };
 
 export default ({ url, title, date, children }: props) => {
     return (
-        <a href={url} className="bg-gray-50 dark:bg-gray-950 dark:text-white flex w-full max-w-4xl h-48 py-5 px-10 flex-col items-start gap-3 rounded-xl border-solid border-2">
+        <a href={url} className="bg-gray-50 dark:bg-gray-950 dark:text-white flex w-11/12 max-w-4xl h-48 py-5 px-10 flex-col items-start gap-3 rounded-xl border-solid border-2">
             <div className="flex flex-col items-start gap-1">
                 <span className="font-sans text-xl font-bold leading-6 tracking-wider">{title}</span>
                 <time className="text-gray-500 font-sans text-xs font-medium leading-4 tracking-wider" dateTime={date.toISOString()}>{new Intl.DateTimeFormat('ja-JP').format(date)}</time>
