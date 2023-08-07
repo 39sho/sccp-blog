d80ce939813fd873548707b3767a2e0f21d7b93f
39sho <g.39sho@gmail.com>
2023-08-07 16:04:45 +0900

    add workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
+import { a } from 'dist/chunks/astro.ee50c925.mjs';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://39sho.github.io",
   base: "/sccp-blog",
   integrations: [react(), tailwind()],
 });
