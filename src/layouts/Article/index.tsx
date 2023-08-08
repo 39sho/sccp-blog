e5b5cb60426aa85ae00a5954f8bc6220387e90a8
39sho <g.39sho@gmail.com>
2023-08-08 19:07:37 +0900

    change design

--- src/layouts/Article/index.tsx
+++ src/layouts/Article/index.tsx
@@ -8,7 +8,7 @@ export default ({ children }: props) => {
         <div className="flex flex-col items-center">
             <Header />
             <Spacer />
-            <main className="flex container flex-col items-center gap-10">
+            <main className="flex container flex-col items-center gap-10 prose dark:prose-invert w-11/12 max-w-4xl">
                 {children}
             </main>
             <Spacer />
